67f6438cb2881bc8d3e029e5fe8a4433ffab2143
ivan [itb] martinez canyero <ivan.martinez.7e6@itb.cat>
2023-03-30 12:48:12 +0200

    bottom navigation

--- app/src/main/java/com/example/shelfie_app/view/MainActivity.kt
+++ app/src/main/java/com/example/shelfie_app/view/MainActivity.kt
@@ -3,16 +3,28 @@ package com.example.shelfie_app.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.shelfie_app.R
+import com.example.shelfie_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
+        val bottomNavigationView = binding.bottomNavigation
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.shelfFragment, R.id.searchFragment,
+            R.id.userProfileFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
 
     }
--- app/src/main/java/com/example/shelfie_app/view/PruebaFragment.kt
+++ app/src/main/java/com/example/shelfie_app/view/PruebaFragment.kt
@@ -1,7 +1,6 @@
 package com.example.shelfie_app.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +8,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.shelfie_app.R
 import com.example.shelfie_app.databinding.FragmentPruebaBinding
 import com.example.shelfie_app.viewmodel.ApiViewModel
-import kotlinx.coroutines.*
-import okhttp3.internal.notify
-import okhttp3.internal.wait
+
 
 
 class PruebaFragment : Fragment() {
